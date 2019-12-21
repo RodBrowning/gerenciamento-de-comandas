c3531ffeff2e4115df44656251643e2f6729c864
RodBrowning <rodrigo.browning@gmail.com>
2019-12-21 00:42:02 -0300

    Adicionado campos ao item de cardápio

--- src/Models/Item.js
+++ src/Models/Item.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 
 const ItemSchema = new mongoose.Schema({
     nome_item: String,
-    preco: Number
+    preco: Number,
+    tipo: {type: String, enum: ["produto","alimento","bebida"], default: "produto"},
+    com_preparo: {type: Boolean, default: false },
+    departamento: {type: String, enum: ["praca","cozinha","bar","churrasqueira","caixa"], default: "praca"},
+    ex_dicas: {type: String, default: 'ex. "Sem cebola", "Copos limpos", "Mande guardanapos", "Capricha no álcool", "etc.."'}
 })
 
 module.exports = mongoose.model("Item", ItemSchema)
