d800a95b00150dc6c079394d0f6761bfbf9376cb
RodBrowning <rodrigo.browning@gmail.com>
2019-12-23 21:05:18 -0300

    Adicionado campo tipo em item

--- src/Models/Item.js
+++ src/Models/Item.js
@@ -4,6 +4,7 @@ const ItemSchema = new mongoose.Schema({
     nome_item: String,
     preco: Number,
     categoria: {type: String, enum: ["produto","alimento","bebida"], default: "produto"},
+    tipo: {type: String, default: null},
     com_preparo: {type: Boolean, default: false },
     departamento: {type: String, enum: ["praca","cozinha","bar","churrasqueira","caixa"], default: "praca"},
     ex_dicas: {type: String, default: 'ex. "Sem cebola", "Copos limpos", "Mande guardanapos", "Capricha no álcool", "etc.."'}
